6a315d16f35250f67e0bf0731969c1ad47b07fc7
Usama Bin Muzaffar <usamamuzaffar@crosshairtechnology.com>
2023-07-10 18:47:06 +0500

    Started implementation of UI Manager
    UI Manager script has been added as a singleton. For now the game starts and the welcome screen is shown and 3 seconds later we are taken to the WannaLogin Screen.

--- Assets/UI_Manager.cs
+++ Assets/UI_Manager.cs
@@ -1,18 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region variables
+
+    #region public-variables
+    
+    public static UI_Manager instance { get; private set; }
+    
+    #endregion
+    
+    #region private-variables
+    
+    [SerializeField] private GameObject welcomScreenPrefab;
+    [SerializeField] private GameObject wannaLoginScreenPrefab;
+    [SerializeField] private Canvas canvas;
+    
+    #endregion
+    
+    #endregion
+
+    #region datastructures
+
+    private Stack<GameObject> backStack = new Stack<GameObject>();
+    
+    #endregion
+
+    #region delegates
+    
+    private delegate void PustToStackDelegate(GameObject add);
+    
+    #endregion
+
+    #region functions
+  
+    private void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+       GameObject loadedScreen = Instantiate(welcomScreenPrefab);
+       loadedScreen.transform.SetParent(canvas.transform, false);
+       Invoke("GoToLogin", 3f);
     }
+    private void GoToLogin()
+    {
+        GameObject loadedScreen = Instantiate(wannaLoginScreenPrefab);
+        loadedScreen.transform.SetParent(canvas.transform, false);
+    }
+
+    #endregion
 }
